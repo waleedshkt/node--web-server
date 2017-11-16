39efd95a31fe6d38e84279603376e9d0c965fac4
waleedshkt <waleedshkt@gmail.com>
2017-11-16 23:49:17 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now} ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (er) => {
+        if(er) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+/*app.use((req, res, next) =>{
+    res.render('maintenance.hbs');
+});*/
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('welcomeMessage', (text) => {
+    return text.toUpperCase();
+});
+
+
+app.get('/', (req, res) => {
+    //res.send('<h1>Hello, Express!</h1>');
+    res.render('home.hbs', {
+        message: 'Welcome to some website!',
+        pageTitle: 'Home Page'
+    });
+    
+});
+app.get('/about', (req, res) => {
+    res.render('about.hbs',{
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        Error: 'Unable to fullfil the request'
+    });
+});
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
